f9cd2e3b8c35dcab81ce712980bf585dc00c4436
DESKTOP-RQ9URRA\Admin <bergado.sylvester@gmail.com>
2023-10-16 23:26:07 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic_sample1',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
